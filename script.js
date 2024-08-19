52bab64d3e97d12f0cc69dea5fdb6945ff1f72db
Moazzam8090 <momoazzam2140@gmail.com>
2024-08-19 11:41:19 +0530

    Your commit message

---
+++ script.js
@@ -0,0 +1,14 @@
+let login = document.querySelector("#login");
+login.addEventListener("click", function(){
+   this.value("login");
+})
+
+let home=document.querySelector("#home");
+home.addEventListener("click", function(){
+   this.value("home");
+})
+ 
+let admsn=document.querySelector("#addmission");
+admsn.addEventListener("click", function(){
+   this.value("admsn");
+})
